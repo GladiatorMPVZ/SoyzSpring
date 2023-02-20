c1cf4e7c3d54f594cefbc9f0230b49ae1e0ee1fe
Leytek <leytek@yandex.ru>
2023-02-21 02:15:59 +0300

    feat: add device api

--- src/client/api.tsx
+++ src/client/api.tsx
@@ -1,11 +1,11 @@
-const fetchJSON = (method, url, data) => {
+const fetchJSON = (method, url, data?) => {
   const options = {
     method,
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json;charset=UTF-8',
     },
-    body: JSON.stringify(data),
+    body: data ? JSON.stringify(data) : '',
   };
   return fetch(url, options).then((res) => res.json());
 };
@@ -21,7 +21,42 @@ const authorize = (username, password) => {
   return fetchJSON('POST', url, { username, password });
 };
 
+const getDevices = () => {
+  const url = 'api/v1/devices';
+
+  return fetchJSON('GET', url);
+};
+
+const getDeviceById = (id) => {
+  const url = `api/v1/devices/${id}`;
+
+  return fetchJSON('GET', url);
+};
+
+const getDeviceByVaporizer = (vaporizerTitle) => {
+  const url = `api/v1/devices/filtered?vaporizerTitle=${vaporizerTitle}`;
+
+  return fetchJSON('GET', url);
+};
+
+const addDevice = (title) => {
+  const url = 'api/v1/devices/';
+
+  return fetchJSON('POST', url, { title });
+};
+
+const deleteDeviceById = (id) => {
+  const url = `api/v1/devices/${id}`;
+
+  return fetchJSON('DELETE', url);
+};
+
 export default {
   register,
   authorize,
+  getDevices,
+  getDeviceById,
+  getDeviceByVaporizer,
+  addDevice,
+  deleteDeviceById,
 };
