c540cc5ddc559b07a8f55222e0cf6ac4ceaef295
Leytek <leytek@yandex.ru>
2023-02-27 23:22:11 +0300

    feat: add Authorization styles

--- src/client/components/Authorization/Authorization.tsx
+++ src/client/components/Authorization/Authorization.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import cn from 'classnames';
 
 import api from '../../api';
 import { TSetAppState } from '../../App';
@@ -54,17 +55,32 @@ const useAuthorization = (props: { setAppState: TSetAppState }) => {
 const AuthorizationView = (props: ReturnType<typeof useAuthorization>) => {
   return (
     <form className="auth__form">
-      <button className="auth__button_set-signIn" type="button" onClick={() => props.setSignIn(true)}>
+      <button
+        className={cn('auth__button_set-signin', { auth__button_active: props.isSignIn })}
+        type="button"
+        onClick={() => props.setSignIn(true)}
+      >
         Вход
       </button>
-      <button className="auth__button_set-signUp" type="button" onClick={() => props.setSignIn(false)}>
+      <button
+        className={cn('auth__button_set-signup', { auth__button_active: !props.isSignIn })}
+        type="button"
+        onClick={() => props.setSignIn(false)}
+      >
         Регистрация
       </button>
-      <h2>{props.isSignIn ? 'Вход' : 'Регистрация'}</h2>
-      {props.auth && <span>{props.errorMessage.current}</span>}
-      <input ref={props.userNameRef} type="text" placeholder="Логин" />
-      <input ref={props.passwordRef} type="text" placeholder="Пароль" />
-      {!props.isSignIn && <input ref={props.confirmPasswordRef} type="text" placeholder="Подтверждение пароля" />}
+      <h2 className="auth__h2">{props.isSignIn ? 'Вход' : 'Регистрация'}</h2>
+      {props.auth && <span className="auth__error">{props.errorMessage.current}</span>}
+      <input className="auth__username" ref={props.userNameRef} type="text" placeholder="Логин" />
+      <input className="auth__password" ref={props.passwordRef} type="text" placeholder="Пароль" />
+      {!props.isSignIn && (
+        <input
+          className="auth__confirm-password"
+          ref={props.confirmPasswordRef}
+          type="text"
+          placeholder="Подтверждение пароля"
+        />
+      )}
       <button className="auth__button_action" type="button" onClick={() => props.action()}>
         {props.isSignIn ? 'Войти' : 'Зарегистрироваться'}
       </button>
