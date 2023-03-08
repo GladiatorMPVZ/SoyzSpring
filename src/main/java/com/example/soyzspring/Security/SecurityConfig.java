eb9308235464dbc54a30bcfd5db9503e0af7eb5a
GladiatorMPVZ <inform.work981@mail.ru>
2023-03-08 15:02:44 +0300

    cors fix

--- src/main/java/com/example/soyzspring/Security/SecurityConfig.java
+++ src/main/java/com/example/soyzspring/Security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -35,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests()
                 .antMatchers("/api/v1/auth").permitAll()
                 .antMatchers("/api/v1/registration").permitAll()
